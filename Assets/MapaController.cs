796a491ed2296325cce9700392a35caf5f0d9c6f
RubyEnoshima <vilaruben70@gmail.com>
2023-01-02 19:56:51 +0100

    Mas cosas de mapa

--- Assets/MapaController.cs
+++ Assets/MapaController.cs
@@ -8,55 +8,84 @@ public class MapaController : MonoBehaviour
     public Tilemap Objetos;
     public Tile Caja;
     public Tile Obstaculo;
-    int nCajas = 20;
+    public Tile Municion;
+    public Tile Bandera;
 
+    Vector3Int esquinaSupIzq = new Vector3Int(-7,6,0);
+    Vector3Int esquinaInfDer = new Vector3Int(6,-7,0);
+
+    Camera mainCamera;
+
+    public int maxCajas = 20;
+    public int minCajas = 4;
+    int nCajas = 0;
+
+
+    bool BanderaSpawneada = false;
+
+
+    /**                  FUNCIONES TILES                    **/
     // Calcula el vector resultante de poner pos en el SR de la esquina superior izquierda
     // Asume que la esquina sup izq es el primer tile muro de Muros
-    private Vector3Int CalcPos(Vector3Int pos){
-        Vector3Int esquina = new Vector3Int(Objetos.origin.x,Objetos.origin.y+Objetos.size.y-1,0);
-        return new Vector3Int(esquina.x+pos.x,esquina.y-pos.y,0);
+    private Vector3Int CalcPos(Vector3 pos){
+        // Vector3Int esquina = new Vector3Int(Objetos.origin.x,Objetos.origin.y+Objetos.size.y-1,0);
+        // return new Vector3Int(esquina.x+pos.x,esquina.y-pos.y,0);
+        return Objetos.WorldToCell(pos);
     }
 
-    public void PonerTile(Vector3Int pos, Tile tile){
+    public void PonerTile(Vector3 pos, Tile tile){
         Objetos.SetTile(CalcPos(pos),tile);
     }
 
-    public void EliminarTile(Vector3Int pos){
+    public void EliminarTile(Vector3 pos){
         PonerTile(pos,null);
     }
 
-    public TileBase ObtTile(Vector3Int pos){
+    public TileBase ObtTile(Vector3 pos){
         return Objetos.GetTile(CalcPos(pos));
     }
 
-    public void SpawnearMunicion(Vector3Int pos){
+    /** --------------------------------------------------- **/
 
+    public void SpawnearMunicion(Vector3 pos){
+        PonerTile(pos,Municion);
     }
 
-    public void DestruirCaja(Vector3Int pos){
+    public void SpawnearBandera(Vector3 pos){
+        PonerTile(pos,Bandera);
+        BanderaSpawneada = true;
+    }
+
+    public void DestruirCaja(Vector3 pos){
         if(ObtTile(pos)==Caja){
             EliminarTile(pos);
             if(Random.Range(0f,1f)>0.65f){
                 SpawnearMunicion(pos);
+            }else if(!BanderaSpawneada && Random.Range(0f,1f)>0.2f){
+                SpawnearBandera(pos);
             }
+            nCajas--;
         }
     }
 
-    public void SpawnearCajas(){
-        //Vector3Int esquina = new Vector3Int(2,1,0);
-        for(int i=0;i<nCajas;i++){
+    // Spawnea n cajas
+    public void SpawnearCajas(int n){
+        //Vector3Int esquina = new Vector3Int(-1,2,0);
+        for(int i=0;i<n;i++){
 
-            int x = Random.Range(2,Objetos.size.x-2);
-            int y = Random.Range(1,Objetos.size.y-1);
+            int x = Random.Range(esquinaSupIzq.x,esquinaInfDer.x+1);
+            int y = Random.Range(esquinaInfDer.y,esquinaSupIzq.y+1);
             
-            // No pongamos 
+            // No pongamos cajas donde ya hayan cosas
+            // FALTA MIRAR QUE NO SE PONGAN ENCIMA DE UN PERSONAJE
             while(ObtTile(new Vector3Int(x,y,0))!=null){
-                x = Random.Range(2,Objetos.size.x-2);
-                y = Random.Range(1,Objetos.size.y-1);
+                x = Random.Range(esquinaSupIzq.x,esquinaInfDer.x+1);
+                y = Random.Range(esquinaInfDer.y,esquinaSupIzq.y+1);
             }
 
             PonerTile(new Vector3Int(x,y,0),Caja);
         }
+        nCajas += n;
     }
 
     
@@ -65,12 +94,25 @@ public class MapaController : MonoBehaviour
     void Start()
     {
         Random.InitState(System.DateTime.Now.Millisecond);
-        SpawnearCajas();
+        SpawnearCajas(maxCajas);
+        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(nCajas==minCajas) SpawnearCajas(minCajas*2);
+
+        // DEBUG
+        if(Input.GetMouseButtonDown(0)){
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(CalcPos(worldPoint));
+            var tile = ObtTile(worldPoint);
+
+            if(tile)
+            {
+                DestruirCaja(worldPoint);
+            }
+        }
     }
 }
