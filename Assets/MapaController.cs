248f8eb4d185e8bfb33b4d9a65236d3d66055849
RubyEnoshima <vilaruben70@gmail.com>
2023-01-02 21:02:48 +0100

    Victoria

--- Assets/MapaController.cs
+++ Assets/MapaController.cs
@@ -6,6 +6,7 @@ using UnityEngine.Tilemaps;
 public class MapaController : MonoBehaviour
 {
     public Tilemap Objetos;
+    public Tilemap Fondo;
     public Tile Caja;
     public Tile Obstaculo;
     public Tile Municion;
@@ -14,14 +15,16 @@ public class MapaController : MonoBehaviour
     Vector3Int esquinaSupIzq = new Vector3Int(-7,6,0);
     Vector3Int esquinaInfDer = new Vector3Int(6,-7,0);
 
-    Camera mainCamera;
-
     public int maxCajas = 20;
     public int minCajas = 4;
     int nCajas = 0;
 
-
     bool BanderaSpawneada = false;
+    bool BanderaAgarrada = false;
+    Vector3 posBandera = new Vector3Int(-1000,-1000,-1000);
+
+    int puntosAzul = 0;
+    int puntosRojo = 0;
 
 
     /**                  FUNCIONES TILES                    **/
@@ -33,42 +36,46 @@ public class MapaController : MonoBehaviour
         return Objetos.WorldToCell(pos);
     }
 
-    public void PonerTile(Vector3 pos, Tile tile){
-        Objetos.SetTile(CalcPos(pos),tile);
+    public void PonerTile(Vector3 pos, Tile tile, Tilemap tilemap){
+        tilemap.SetTile(CalcPos(pos),tile);
     }
 
-    public void EliminarTile(Vector3 pos){
-        PonerTile(pos,null);
+    public void EliminarTile(Vector3 pos, Tilemap tilemap){
+        PonerTile(pos,null,tilemap);
     }
 
-    public TileBase ObtTile(Vector3 pos){
-        return Objetos.GetTile(CalcPos(pos));
+    public TileBase ObtTile(Vector3 pos,Tilemap tilemap){
+        return tilemap.GetTile(CalcPos(pos));
     }
 
     /** --------------------------------------------------- **/
 
     public void SpawnearMunicion(Vector3 pos){
-        PonerTile(pos,Municion);
+        PonerTile(pos,Municion,Objetos);
     }
 
     public void SpawnearBandera(Vector3 pos){
-        PonerTile(pos,Bandera);
+        PonerTile(pos,Bandera,Objetos);
         BanderaSpawneada = true;
+        posBandera = pos;
     }
 
     public void DestruirCaja(Vector3 pos){
-        if(ObtTile(pos)==Caja){
-            EliminarTile(pos);
+        if(ObtTile(pos,Objetos)==Caja){
+            EliminarTile(pos,Objetos);
+
+            // Decide si poner municion o una bandera y spawnea
             if(Random.Range(0f,1f)>0.65f){
                 SpawnearMunicion(pos);
             }else if(!BanderaSpawneada && Random.Range(0f,1f)>0.2f){
                 SpawnearBandera(pos);
             }
+
             nCajas--;
         }
     }
 
-    // Spawnea n cajas
+    // Spawnea n cajas en el mapa
     public void SpawnearCajas(int n){
         //Vector3Int esquina = new Vector3Int(-1,2,0);
         for(int i=0;i<n;i++){
@@ -78,16 +85,32 @@ public class MapaController : MonoBehaviour
             
             // No pongamos cajas donde ya hayan cosas
             // FALTA MIRAR QUE NO SE PONGAN ENCIMA DE UN PERSONAJE
-            while(ObtTile(new Vector3Int(x,y,0))!=null){
+            while(ObtTile(new Vector3Int(x,y,0),Objetos)!=null){
                 x = Random.Range(esquinaSupIzq.x,esquinaInfDer.x+1);
                 y = Random.Range(esquinaInfDer.y,esquinaSupIzq.y+1);
             }
 
-            PonerTile(new Vector3Int(x,y,0),Caja);
+            PonerTile(new Vector3Int(x,y,0),Caja,Objetos);
         }
         nCajas += n;
     }
 
+    void ComprobarVictoria(){
+        if(BanderaSpawneada && !BanderaAgarrada){
+            TileBase t = ObtTile(posBandera,Fondo);
+            if(t.name=="Azul"){
+                puntosAzul++;
+
+                EliminarTile(posBandera,Objetos);
+                SpawnearCajas(maxCajas-nCajas);
+            }else if(t.name=="Rojo"){
+                puntosRojo++;
+
+                EliminarTile(posBandera,Objetos);
+                SpawnearCajas(maxCajas-nCajas);
+            }  
+        }
+    }
     
 
     // Start is called before the first frame update
@@ -95,19 +118,21 @@ public class MapaController : MonoBehaviour
     {
         Random.InitState(System.DateTime.Now.Millisecond);
         SpawnearCajas(maxCajas);
-        mainCamera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Respawnear cajas
         if(nCajas==minCajas) SpawnearCajas(minCajas*2);
+
+        ComprobarVictoria();
 
         // DEBUG
         if(Input.GetMouseButtonDown(0)){
             Vector3 worldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Debug.Log(CalcPos(worldPoint));
-            var tile = ObtTile(worldPoint);
+            var tile = ObtTile(worldPoint,Objetos);
 
             if(tile)
             {
